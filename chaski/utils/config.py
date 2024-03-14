d1790efc506e38bded89d8d9c30db9da644004f5
enzokro <christopher.kroenke@gmail.com>
2024-03-14 18:57:20 -0400

    better loggin

--- chaski/utils/config.py
+++ chaski/utils/config.py
@@ -1,17 +1,25 @@
 import os
 
 class Config:
+    """Configuration for the chaski library."""
+
+    # default options for serving the web app
     HOST = os.environ.get("HOST", "0.0.0.0")
     PORT = int(os.environ.get("PORT", 8000))
+
+    # NOTE: hardcoded to my model paths
     MODEL_PATH = os.environ.get(
         "MODEL_PATH",
         "/Users/cck/repos/llama.cpp/models/Mistral-7B-Instruct-v0.2/mistral-7b-instruct-v0.2.Q4_K_S.gguf",
     )
 
+    # whether or not to use the embeddings
     USE_EMBEDDINGS = False
 
+    # maximum number of output tokens
     MAX_TOKENS = 256
 
+    # default options for the embedding model
     DEFAULT_EMBEDDINGS = {
         'model_name': 'sentence-transformers/all-MiniLM-L6-v2',
         'model_library': 'sentence-transformers',
--- chaski/utils/logging.py
+++ chaski/utils/logging.py
@@ -1,50 +1,85 @@
 import logging
 import sys
+from fastcore.basics import store_attr
 from logging.handlers import TimedRotatingFileHandler
 
-def setup_logging(
-        log_level: str = "INFO", 
-        log_file: str = None, 
+class Logger:
+    """Manages the logging configuration for the application.
+    
+    NOTE: meant to be used when we are running in the cloud.
+    """
+
+    def __init__(
+        self,
+        log_level: str = "INFO",
+        log_file: str = None,
         log_rotation: bool = True,
         log_retention: int = 60,
+        log_format: str = "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        log_date_format: str = "%Y-%m-%d %H:%M:%S",
+        do_setup: bool = True,
     ):
-    """Sets up logging for the application.
+        """
+        Initializes the LoggingManager.
 
-    Args:
-        log_level (str): The minimum log level to capture (default: "INFO").
-        log_file (str): The path to the log file (default: None, logs to console only).
-        log_rotation (bool): Whether to enable log rotation (default: True).
-        log_retention (int): The number of days to retain log files (default: 60).
+        Args:
+            log_level (str): The minimum log level to capture (default: "INFO").
+            log_file (str): The path to the log file (default: None, logs to console only).
+            log_rotation (bool): Whether to enable log rotation (default: True).
+            log_retention (int): The number of days to retain log files (default: 60).
 
-    Examples:
-        from chaski.utils.logging_utils import setup_logging
-        setup_logging(log_level="INFO", log_file="app.log", log_rotation=True, log_retention=60)
-    """
-    log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
-    log_date_format = "%Y-%m-%d %H:%M:%S"
+        Example:
+            # import the 
+            from chaski.utils.logging import Logger
 
-    # Set up the root logger
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.getLevelName(log_level))
+            # Set up logging
+            logging_manager = Logger(log_level="INFO", log_file="app.log", log_rotation=True, log_retention=60)
 
-    # Create a console handler
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.getLevelName(log_level))
-    console_formatter = logging.Formatter(log_format, datefmt=log_date_format)
-    console_handler.setFormatter(console_formatter)
-    root_logger.addHandler(console_handler)
+            # Create a logger for the current module
+            logger = Logger.get_logger(__name__)
+        """
+        store_attr()
+        if do_setup: self._setup_logging()
 
-    if log_file:
-        # Create a file handler
-        if log_rotation:
-            file_handler = TimedRotatingFileHandler(log_file, when="midnight", backupCount=log_retention)
-        else:
-            file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(logging.getLevelName(log_level))
-        file_formatter = logging.Formatter(log_format, datefmt=log_date_format)
-        file_handler.setFormatter(file_formatter)
-        root_logger.addHandler(file_handler)
+    def _setup_logging(self):
+        """Sets up the logging configuration."""
+        # Set up the root logger
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.getLevelName(self.log_level))
 
-    # Disable logging for third-party libraries
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    logging.getLogger("requests").setLevel(logging.WARNING)
+        # Create a console handler
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.getLevelName(self.log_level))
+        console_formatter = logging.Formatter(self.log_format, datefmt=self.log_date_format)
+        console_handler.setFormatter(console_formatter)
+        root_logger.addHandler(console_handler)
+
+        if self.log_file:
+            # Create a file handler
+            if self.log_rotation:
+                file_handler = TimedRotatingFileHandler(
+                    self.log_file, when="midnight", backupCount=self.log_retention
+                )
+            else:
+                file_handler = logging.FileHandler(self.log_file)
+            file_handler.setLevel(logging.getLevelName(self.log_level))
+            file_formatter = logging.Formatter(self.log_format, datefmt=self.log_date_format)
+            file_handler.setFormatter(file_formatter)
+            root_logger.addHandler(file_handler)
+
+        # Disable logging for third-party libraries
+        logging.getLogger("urllib3").setLevel(logging.WARNING)
+        logging.getLogger("requests").setLevel(logging.WARNING)
+
+    @staticmethod
+    def get_logger(name: str) -> logging.Logger:
+        """
+        Returns a logger with the specified name.
+
+        Args:
+            name (str): The name of the logger.
+
+        Returns:
+            logging.Logger: The logger instance.
+        """
+        return logging.getLogger(name)
